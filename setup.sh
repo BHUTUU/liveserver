1410c520446ee11cacff67a9c75f190843fc8096
BHUTUU <raj259942@gmail.com>
2021-09-05 16:35:06 +0530

    removed aarch64 conditinal statement

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
-DIST=$(uname -m)
-if [[ $DIST != 'aarch64' ]]; then
-printf "\e[32m[\e[31m!\e[32m]\e[31mSorry, but your system is not compatible!!\e[0m\n"
-exit 1
-fi
+#DIST=$(uname -m)
+#if [[ $DIST != 'aarch64' ]]; then
+#printf "\e[32m[\e[31m!\e[32m]\e[31mSorry, but your system is not compatible!!\e[0m\n"
+#exit 1
+#fi
 printf "\e[32mInstalling requirements\e[0m\n"
 if ! hash php > /dev/null 2>&1; then
 apt install php -y > /dev/null 2>&1
